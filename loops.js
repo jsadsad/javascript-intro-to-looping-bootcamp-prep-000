5605622d35ec7cbe205f6cbc45160d6fe8e1fdf4
JCS <joshsad2@gmail.com>
2017-09-14 05:39:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,3 @@
 function forLoop(array) {
-  for (var i = 0; i <= 25; i++) {
-    if (i === 1) {
-      return `I am 1 strange loop`;
-    } else {
-      return `I am ${i} strange loops`;
-    }
-  }
+
 }
